49375ae81fe37c2b290ba8a80a02dc7511692672
prmm08 <elcatalan77@gmail.com>
2025-12-07 22:31:01 +0100

    22:30

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import os
 from flask import Flask, request, jsonify
 
 # -------- API Keys --------
-API_KEY = os.getenv("BINGX_API_KEY", "DEIN_API_KEY")
-API_SECRET = os.getenv("BINGX_API_SECRET", "DEIN_API_SECRET")
+API_KEY = "XeyESAWMvOPHPPlteKkem15yGzEPvHauxKj5LORpjrvOipxPza5DiWkGSMJGhWZyIKp0ZNQwhN17R3aon1RA"
+API_SECRET = "EKHC1rgjFzQVBO9noJa1CHaeoh9vJqv78EXg76aqozvejJbTknkaVr2G3fJyUcBZs1rCoSRA5vMQ6gZYmIg"
 
 BINGX_BASE = "https://open-api.bingx.com"
 
