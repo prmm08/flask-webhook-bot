20d83f987cbd35643696c2dce8142a70fab6e0a3
prmm08 <elcatalan77@gmail.com>
2025-12-09 20:39:58 +0100

    9.12-20:39

--- app.py
+++ app.py
@@ -97,8 +97,8 @@ def handle_alert():
         side = "SELL"
         size = 20
         leverage = 20
-        tp_percent = 2
-        sl_percent = 100
+        tp_percent = 3
+        sl_percent = 1.5
 
         price = get_price(symbol)
         qty = round(size / price, 6)
