bbe5f7e730aae80881ecf62f465380779b94aad1
prmm08 <elcatalan77@gmail.com>
2025-12-14 18:33:26 +0100

    14.12-18:33

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def execute_long_bingx(symbol):
     trade_size_usdt = 20 # Positionsgröße in USDT
     leverage = 20
     tp_percent = 4
-    sl_percent = 2
+    sl_percent = 1
     
     qty = round(trade_size_usdt / price, 6)
     ts = str(int(time.time() * 1000))
