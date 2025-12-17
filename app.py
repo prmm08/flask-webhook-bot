1bbdc9bb2383490959c4133f9efd9bdd6619cb7f
prmm08 <elcatalan77@gmail.com>
2025-12-17 07:59:25 +0100

    17.12-07:40

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 
 # Globale Settings
 RSI_TIMEFRAME = "1m"
-TP_PERCENT, SL_PERCENT, BE_PERCENT = 0.25, 0.5, 0.4
+TP_PERCENT, SL_PERCENT, BE_PERCENT = 0.1, 0.25, 0.4
 
 # ---------------- SIGNING (FIXED) ----------------
 
