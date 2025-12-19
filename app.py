2e85979bb4616e8e87564e267f67ad2550ab7a88
prmm08 <elcatalan77@gmail.com>
2025-12-19 06:56:39 +0100

    19.12-06:56

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ app = Flask(__name__)
 
 # Globale Settings
 RSI_TIMEFRAME = "1m" # Wird nur noch zur Info geladen
-BTC_TREND_TIMEFRAME = "1h"
-MA_PERIOD = 50
-TP_PERCENT, SL_PERCENT, BE_PERCENT = 1.5, 1.5, 0.5
+BTC_TREND_TIMEFRAME = "15m"
+MA_PERIOD = 15
+TP_PERCENT, SL_PERCENT, BE_PERCENT = 1.5, 1.5, 1.5
 
 # ---------------- SIGNING & HELPERS ----------------
 
