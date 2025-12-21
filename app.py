20a19af4192f548ae3c1bdb351034baab244ac0f
prmm08 <elcatalan77@gmail.com>
2025-12-21 22:51:14 +0100

    21.12-22:51

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 
 # Globale Settings
 RSI_TIMEFRAME = "1m"
-TP_PERCENT, SL_PERCENT, BE_PERCENT = 1.5, 1.5, 1.5
+TP_PERCENT, SL_PERCENT, BE_PERCENT = 1.5, 3.0, 1.5
 TRADE_SIZE = 10  # USDT
 LEVERAGE = 10
 
