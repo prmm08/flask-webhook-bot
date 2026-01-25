8f4106960de555142e271ecc5038b23569866b47
prmm08 <elcatalan77@gmail.com>
2026-01-25 23:33:30 +0100

    25.01-23:25

--- app.py
+++ app.py
@@ -31,9 +31,9 @@ is_synced = False
 # --- STRATEGIE EINSTELLUNGEN ---
 TP_MODE = "AVERAGE"        
 USE_SL = False              
-SL_PERCENT = 40.0          
+SL_PERCENT = 0.5        
 TP_PERCENT = 0.5          
-BE_DCA_LEVEL = 3           
+BE_DCA_LEVEL = 1           
 BE_PROFIT_PERCENT = 0.05   
 
 DCA_COUNT = 4          
