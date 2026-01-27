f90a7beca0b29155ce1e235d2310ad8163fa143b
prmm08 <elcatalan77@gmail.com>
2026-01-27 16:17:48 +0100

    26.01-16:17

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ is_synced = False
 TP_MODE = "AVERAGE"        # "AVERAGE" or "FIRST"
 USE_SL = False
 SL_PERCENT = 2.5
-TP_PERCENT = 0.05
+TP_PERCENT = 0.5
 BE_DCA_LEVEL = 3
 BE_PROFIT_PERCENT = 0.05
 
