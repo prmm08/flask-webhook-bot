f5f2d36f1d897e770fa8d5a5e19c98b24ddb0230
prmm08 <elcatalan77@gmail.com>
2026-01-31 05:47:34 +0100

    30.01-05:44

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def webhook():
         "symbol": symbol,
         "direction": str(data.get("direction", "LONG")).upper(),
         "leverage": int(data.get("leverage", 20)),
-        "trade_size": float(data.get("trade_size", 200)),
+        "trade_size": float(data.get("trade_size", 100)),
         "tp_percent": float(data.get("tp_percent", 0.5)),
         "sl_percent": float(data.get("sl_percent", 50.0))
     }
